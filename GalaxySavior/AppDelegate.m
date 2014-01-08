546df9608ec319c305f89bcac96fe373aee81a35
r-sreesaran <r.sreesaran@gmail.com>
2014-01-09 01:55:48 +0530

    Initial Commit

---
+++ GalaxySavior/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  GalaxySavior
+//
+//  Created by sree on 09/01/14.
+//  Copyright (c) 2014 sree. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ GalaxySavior/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GalaxySavior
+//
+//  Created by sree on 09/01/14.
+//  Copyright (c) 2014 sree. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
