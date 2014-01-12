2fa18f48606fc3e34bbb432f3280dc54b1f1cafa
r-sreesaran <r.sreesaran@gmail.com>
2014-01-12 17:18:12 +0530

    Firing missiles

--- GalaxySavior/galaxysaviour.c
+++ GalaxySavior/galaxysaviour.c
@@ -15,8 +15,11 @@ const int SCREEN_H = 400;
 const int x=0;
 const int y=0;
 const int FPS = 60;
-enum MKEYS{KEY_UP,KEY_DOWN,KEY_LEFT,KEY_RIGHT};
-bool keys[4]= {false,false,false,false};
+const int num_bullets=5;
+const bool GameOver= false;
+
+enum MKEYS{KEY_UP,KEY_DOWN,KEY_LEFT,KEY_RIGHT,KEY_SPACEBAR};
+bool keys[5]= {false,false,false,false,false};
 
 void init_ship(SpaceShip *ship);
 void draw_ship(SpaceShip *ship);
@@ -25,6 +28,10 @@ void move_ship_right(SpaceShip *ship);
 void move_ship_up(SpaceShip *ship);
 void move_ship_down(SpaceShip *ship);
 
+void init_bullet(Bullet bullet[]);
+void draw_bullet(Bullet bullet[]);
+void fire_bullet(Bullet bullet[],SpaceShip *ship);
+void update_bullet(Bullet bullet[]);
 
 int main(void)
 {
@@ -34,7 +41,7 @@ int main(void)
     ALLEGRO_TIMER *timer =NULL;
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
     SpaceShip ship;
-    
+    Bullet bullet[num_bullets];
     if(!al_init())
     {
         fprintf(stderr,"allegro is not intialized");
@@ -67,6 +74,7 @@ int main(void)
     al_register_event_source(event_queue,al_get_keyboard_event_source());
     al_register_event_source(event_queue,al_get_display_event_source(display));
     init_ship(&ship);
+    init_bullet(&bullet);
     al_start_timer(timer);
     while(!done)
     {
@@ -86,10 +94,20 @@ int main(void)
             }if(keys[KEY_LEFT])
             {
                 move_ship_left(&ship);
-            }if(keys[KEY_RIGHT])
+            }
+            if(keys[KEY_RIGHT])
             {
                 move_ship_right(&ship);
             }
+            if(keys[KEY_SPACEBAR])
+            {
+                fire_bullet(&bullet,&ship);
+            }
+            if(!GameOver)
+            {
+                update_bullet(&bullet);
+            }
+            
             redraw = true;
         }
         else if(ev.type==ALLEGRO_EVENT_DISPLAY_CLOSE)
@@ -120,6 +138,11 @@ int main(void)
                     keys[KEY_RIGHT]=true;
                     break;
                 }
+                case ALLEGRO_KEY_SPACE:
+                {
+                    keys[KEY_SPACEBAR] = true;
+                    break;
+                }
                     
             }
         }
@@ -147,6 +170,11 @@ int main(void)
                     keys[KEY_RIGHT]=false;
                     break;
                 }
+                case ALLEGRO_KEY_SPACE:
+                {
+                    keys[KEY_SPACEBAR]=false;
+                    break;
+                }
                     
             }
         }
@@ -156,8 +184,9 @@ int main(void)
             redraw = false ;
             al_clear_to_color(al_map_rgb(255,100,100));
             draw_ship(&ship);
+            draw_bullet(bullet);
             al_flip_display();
-            
+            al_clear_to_color(al_map_rgb(255,100,100));
             
             
         }
@@ -225,3 +254,55 @@ void move_ship_down(SpaceShip *ship)
     }
     
 }
+
+void init_bullet(Bullet *bullet)
+{
+    for (int i=0;i<5;i++)
+    {
+    bullet[i].ID=BULLET;
+	bullet[i].live = false;
+	bullet[i].speed=6;
+
+    }
+}
+
+void fire_bullet(Bullet *bullet,SpaceShip *ship)
+{
+    for (int i=0;i<5;i++)
+    {
+    if(!bullet[i].live)
+    {
+        bullet[i].x = ship->x;
+        bullet[i].y = ship->y-20;
+        bullet[i].live = true;
+        break;
+     }
+    }
+}
+
+void draw_bullet(Bullet bullet[])
+{
+    for (int i=0;i<5;i++)
+    {
+    if(bullet[i].live)
+    {
+      //  al_draw_filled_rectangle(bullet[i].x,bullet[i].y,bullet[i].x,bullet[i].y+3,al_map_rgb(0,0,0));
+        al_draw_filled_circle(bullet[i].x,bullet[i].y,2,al_map_rgb(100,100,100));
+       // fprintf(stderr," %d) the co-ordinates of x %d and the y position %d ",i,bullet[i].x,bullet[i].y);
+    }
+    }
+}
+
+void update_bullet(Bullet *bullet)
+{
+    for(int i = 0; i < 5; i++)
+	{
+		if(bullet[i].live)
+		{
+			bullet[i].y -= bullet[i].speed;
+			if(bullet[i].y<0)
+				bullet[i].live = false;
+		}
+	}
+}
+
